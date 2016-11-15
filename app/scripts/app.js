ac54a69e1349bf1d678c7aaddb1ec37125131d3a
Austin James Rea <austinjrea@gmail.com>
2016-11-15 17:58:46 +0000

    added css styling to messages and removed surplus files

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -25,6 +25,6 @@
 	}
 
 	angular
-		.module('blocChat', ['firebase', 'ui.router', 'ui.bootstrap'])
+		.module('blocChat', ['firebase', 'ui.router', 'ui.bootstrap', 'ngCookies'])
 		.config(config);
 })();
